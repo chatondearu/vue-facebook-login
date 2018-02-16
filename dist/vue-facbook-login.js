dc8aa6788d5b31adee9ecc69b59ba3c43b79783a
Romain Lienard <romain.lienard.web@gmail.com>
2018-02-16 16:14:58 +0100

    first commit

---
+++ dist/vue-facbook-login.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// src/main.js
+
+var main = {
+  toto: function toto() {
+    console.log('first commit');
+  }
+};
+
+module.exports = main;
+//# sourceMappingURL=vue-facbook-login.js.map
